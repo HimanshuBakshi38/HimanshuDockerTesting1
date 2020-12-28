b272b2d433535c7fcb33078726f2dd5be89b70c8
himanshubakshi38 <himanshubakshi38@gmail.com>
2020-12-28 10:08:15 +0530

    Testqa2

--- login.java
+++ login.java
@@ -3,9 +3,9 @@ package Fabulate;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.ClickAction;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -256,27 +256,9 @@ public class login {
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	@AfterMethod
 	public void afterMethod() {
 	}
-
+	
+	
 }
